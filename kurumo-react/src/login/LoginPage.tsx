e772a684be4449ee4e7eb040780bd29c180462a3
ushi <>
2024-06-29 16:31:06 +0900

    feat: registerページ編集

--- kurumo-react/src/login/LoginPage.tsx
+++ kurumo-react/src/login/LoginPage.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
                     </div>
                 </div>
             </body>
-            <div className="mx-auto w-80 h-100 bg-green-200">
+            <div className="mx-auto w-80 h-100">
                 <img className="mb-10" src={base_logo} alt="Description of image" />
                 <form >
                     <FormComponent text={"メールアドレス"} label={"mail"}></FormComponent>
@@ -33,8 +33,7 @@ export default function LoginPage() {
                 <ButtonComponent label={"ログイン"} onButtonClick={handleSelectedRegisterPage}></ButtonComponent>
                 <div className="flex-1 border-b border-gray-500 mb-20"></div>
                 <div className="text-center">または </div>
-                <ButtonComponent label={"新規登録"} onButtonClick={handleSelectedRegisterPage}></ButtonComponent>
-
+                <ButtonComponent label={"新規登録"} onButtonClick={handleLogin}></ButtonComponent>
             </div>
         </>
     );
--- kurumo-react/src/register/RegisterPage.tsx
+++ kurumo-react/src/register/RegisterPage.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import FormComponent from "../components/FormComponent";
+import ButtonComponent from "../components/ButtonComponent";
 
 export default function RegisterPage() {
     const navigate = useNavigate();
@@ -10,9 +12,17 @@ export default function RegisterPage() {
     }
     return (
         <>
-            <div>
-                新規登録
+            <body className="bg-blue-500">
+                <div className="flex flex-col h-10">
+                    <div className="bg-blue-500 sticky top-0">
+                    </div>
+                </div>
+            </body>
+            <div className="mx-auto w-80 h-100 ">
+                <FormComponent text={"メールアドレス"} label={"mail"}></FormComponent>
+                <ButtonComponent label={"新規登録"} onButtonClick={handleSelectedRegisterPage}></ButtonComponent>
             </div>
+
         </>
     );
 }
